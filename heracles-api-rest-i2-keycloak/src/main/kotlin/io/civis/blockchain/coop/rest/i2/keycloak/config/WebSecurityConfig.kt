4d51f1948f5fcddcdf1dec89dc8eb613d1e43c8c
Luc Heydel <luc@smartb.city>
2021-02-16 14:51:27 +0100

    authentication filter on custom jwt properties

--- heracles-api-rest-i2-keycloak/src/main/kotlin/io/civis/blockchain/coop/rest/i2/keycloak/config/WebSecurityConfig.kt
+++ heracles-api-rest-i2-keycloak/src/main/kotlin/io/civis/blockchain/coop/rest/i2/keycloak/config/WebSecurityConfig.kt
@@ -2,10 +2,18 @@ package io.civis.blockchain.coop.rest.i2.keycloak.config
 
 import org.springframework.boot.autoconfigure.AutoConfigureBefore
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.PropertySource
+import org.springframework.security.authorization.AuthorizationDecision
 import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.core.Authentication
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken
 import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.security.web.server.authorization.AuthorizationContext
+import reactor.core.publisher.Mono
+import java.util.HashMap
 
 @AutoConfigureBefore(ConditionalReactiveOAuth2ResourceServerAutoConfiguration::class)
 @PropertySource("classpath:i2.properties")
@@ -16,21 +24,37 @@ class WebSecurityConfig {
     }
 
     @Bean
+    @ConfigurationProperties(prefix = "i2.filter")
+    fun authFilter(): Map<String, String> = HashMap()
+
+    @Bean("springSecurityFilterChain")
     @ConditionalOnProperty(conditionalProperty, matchIfMissing = true, havingValue = "false")
     fun dummyAuthenticationProvider(http: ServerHttpSecurity): SecurityWebFilterChain {
         http.authorizeExchange().anyExchange().permitAll()
         return http.build()
     }
 
-    @Bean
-    @ConditionalOnProperty(conditionalProperty, havingValue = "server")
+    @Bean("springSecurityFilterChain")
+    @ConditionalOnProperty(conditionalProperty)
     fun oauthAuthenticationProvider(http: ServerHttpSecurity): SecurityWebFilterChain {
         http.authorizeExchange()
                 .anyExchange()
-                .authenticated()
+                .access(::authenticate)
                 .and()
                 .oauth2ResourceServer()
                 .jwt()
         return http.build()
+
+    }
+
+    private fun authenticate(authentication: Mono<Authentication>, context: AuthorizationContext): Mono<AuthorizationDecision> {
+        return authentication.map { auth ->
+            if (auth !is JwtAuthenticationToken || auth.token == null) {
+                return@map false
+            }
+
+            val filters = authFilter()
+            filters.isEmpty() || filters.all { (key, value) -> auth.token.claims[key] == value }
+        }.map(::AuthorizationDecision)
     }
 }
